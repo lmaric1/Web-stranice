56ce64c5a60fce09256373fa985845aa838ee1ce
lmaric1 <93379077+lmaric1@users.noreply.github.com>
2023-06-20 10:53:33 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,54 @@
+$(document).foundation()
+
+$('#naslov').click(function(){
+
+    $('#naslov').html('ZLATNA GROZNICA');
+    $('#naslov').css('color', 'red');
+
+    return false;
+});
+
+
+
+$( "#cro" ).click(function() {
+    $( "#p1" ).show( "slow", function() {
+        return false;
+    });    });
+    $( "#isl" ).mouseenter(function() {
+        $( "#p2" ).show( "slow", function() {
+            return false;
+        });    });
+        $( "#egy" ).dblclick(function() {
+            $( "#p3" ).show( "slow", function() {
+                return false;
+            });    });
+            $( "#jap" ).click(function() {
+                $( "#p4" ).show( "slow", function() {
+                    return false;
+                });    });
+               
+                   
+                    $( "#cro" ).dblclick(function() {
+                        $( "#p6" ).show( "slow", function() {
+                            return false;
+                        });    });
+                     
+                      
+                            $( "#isl" ).mouseleave(function() {
+                                $( "#p8" ).show( "slow", function() {
+                                    return false;
+                                });    });
+
+                                $( "#iks" ).click(function() {
+                                    $( "#p10" ).show( "slow", function() {
+                                        return false;
+                                    });    });
+
+                                    $('#iks').click(function(){
+
+                                        $('#iks').css('color', 'red',);
+                                     
+                                       
+                                    
+                                        return false;
+                                    });
